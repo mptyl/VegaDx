f434704abef491b671966d17316a1b71993c4e65
mptyl <Poldo2019!>
2023-03-06 11:55:57 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, ResetPasswordFormModule, CreateAccountFormModule, ChangePasswordFormModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { UnauthenticatedContentModule } from './unauthenticated-content';
+import { AppRoutingModule } from './app-routing.module';
+import { LanguagePanelComponent } from './shared/components/language-panel/language-panel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LanguagePanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    ResetPasswordFormModule,
+    CreateAccountFormModule,
+    ChangePasswordFormModule,
+    LoginFormModule,
+    UnauthenticatedContentModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AuthService,
+    ScreenService,
+    AppInfoService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/language-panel/language-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-panel',
+  templateUrl: './language-panel.component.html',
+  styleUrls: ['./language-panel.component.scss']
+})
+export class LanguagePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
